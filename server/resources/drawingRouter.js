a5f4e03f1f10a9ec3806284e2d0572f6ccdf3dae
Andrew Sutedja <andrew.sutedja@gmail.com>
2016-10-02 18:29:12 -0700

    Rebase

--- server/resources/drawingRouter.js
+++
@@ -1,8 +0,0 @@
-var drawingRouter = require('express').Router();
-var drawingController = require('./drawingController');
-
-drawingRouter.route('/:roundId')
-  .get(drawingController.retrieveRoundsDrawings)
-  .post(drawingController.addDrawing);
-
-module.exports = drawingRouter;
